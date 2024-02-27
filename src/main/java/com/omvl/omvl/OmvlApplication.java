9d05486d1dbe096015a87cd52acba84727007f92
seok5182 <seok5182@naver.com>
2024-02-27 15:42:17 +0900

    프로젝트 시작

---
+++ src/main/java/com/omvl/omvl/OmvlApplication.java
@@ -0,0 +1,13 @@
+package com.omvl.omvl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmvlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmvlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omvl/omvl/OmvlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omvl.omvl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmvlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
